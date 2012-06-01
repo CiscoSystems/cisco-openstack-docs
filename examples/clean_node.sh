8b6f12fb67ad22d90942f9a1129dac02bb562a64
Robert Starmer <starmer@cisco.com>
2012-06-01 23:52:18 +0000

    added an additional ssh key removal for the clean script

--- examples/clean_node.sh
+++ examples/clean_node.sh
@@ -5,3 +5,4 @@ sudo cobbler system poweroff --name=$1
 sudo cobbler system poweron --name=$1
 sudo puppet cert clean $1.$domain
 sudo ssh-keygen -R $1
+sudo ssh-keygen -R `host $1 | awk '{print \$4}'`
