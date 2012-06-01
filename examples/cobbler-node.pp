c0ac5c37ce1062d580319bc68bb4b29f83c509d7
Robert Starmer <starmer@cisco.com>
2012-06-01 13:38:51 -0700

    example manifests in /examples for OS_Simple.md

---
+++ examples/cobbler-node.pp
@@ -0,0 +1,92 @@
+# = Cobbler and Puppet for nodes
+#
+# == Example
+#
+# add this to your site.pp file:
+# import "cobbler-node"
+# in your site.pp file, add a node definition like:
+# node 'cobbler.example.com' inherits cobbler-node { }
+#
+
+# A node definition for cobbler
+# You will likely also want to change the IP addresses, domain name, and perhaps
+# even the proxy address
+# If you are not using UCS blades, don't worry about the org-EXAMPLE, and if you are
+# and aren't using an organization domain, just leave the value as ""
+# An example MD5 crypted password is ubuntu: $6$UfgWxrIv$k4KfzAEMqMg.fppmSOTd0usI4j6gfjs0962.JXsoJRWa5wMz8yQk4SfInn4.WZ3L/MCt5u.62tHDGB36EhiKF1
+# which is used by the cobbler preseed file to set up the default admin user.
+node /cobbler-node/ {
+
+# class { puppet:
+#  run_master => true,
+#  puppetmaster_address => "sdu-os-0.sdu.lab",
+# }
+
+ class { cobbler:
+  node_subnet => "192.168.100.0",
+  node_netmask => "255.255.255.0",
+  node_gateway => "192.168.100.1",
+  node_dns => "192.168.26.186",
+  ip => '192.168.100.254',
+  dns_service => 'dnsmasq',
+  dhcp_service => 'dnsmasq',
+  dhcp_ip_low => '192.168.100.50',
+  dhcp_ip_high => '192.168.100.59',
+  domain_name => "sdu.lab",
+  proxy => "http://192.168.26.163:3142/",
+  password_crypted => '$6$UfgWxrIv$k4KfzAEMqMg.fppmSOTd0usI4j6gfjs0962.JXsoJRWa5wMz8yQk4SfInn4.WZ3L/MCt5u.62tHDGB36EhiKF1',
+ }
+
+# This will load the Ubuntu precise x86_64 server iso into cobbler
+ cobbler::ubuntu { "precise":
+ }
+
+# This will build a preseed file called 'cisco-preseed' in /etc/cobbler/preseeds/
+ cobbler::ubuntu::preseed { "cisco-preseed":
+  packages => "openssh-server lvm2 libvirt-bin ntp puppet kvm",
+  late_command => '
+sed -e "/logdir/ a pluginsync=true" -i /target/etc/puppet/puppet.conf ; \
+sed -e "s/START=no/START=yes/" -i /target/etc/default/puppet ; \
+echo "server ${http_server} iburst" > /target/etc/ntp.conf ; \
+echo "auto eth1" >> /target/etc/network/interfaces ; \
+echo "iface eth1 inet loopback" >> /target/etc/network/interfaces
+',
+  proxy => 'http://128.107.252.163:3142/',
+  password_crypted => '$6$5NP1.NbW$WOXi0W1eXf9GOc0uThT5pBNZHqDH9JNczVjt9nzFsH7IkJdkUpLeuvBU.Zs9x3P6LBGKQh6b0zuR8XSlmcuGn.',
+  expert_disk => true,
+  diskpart => ['/dev/sdc'],
+  boot_disk => '/dev/sdc',
+ }
+
+
+# The following are node definitions that will allow cobbler to PXE boot the hypervisor OS onto the system (based on the preseed built above)
+# You will want to adjust the "title" (maps to system name in cobbler), mac address (this is the PXEboot MAC target), IP (this is a static DHCP delivered address for this particular node), domain (added to /etc/resolv.conf for proper function), power address, the same one for power-strip based power control, per-node for IPMI/CIMC/ILO based control, power-ID needs to map to power port or service profile name (in UCSM based deployements)
+cobbler::node { "control-1":
+ mac => "00:25:b5:00:00:01",
+ profile => "precise-x86_64-auto",
+ ip => "192.168.100.10",
+ domain => "example.lab",
+ preseed => "/etc/cobbler/preseeds/cisco-preseed",
+ power_address => "192.168.100.5:org-EXAMPLE",
+ power_type => "ucs",
+ power_user => "admin",
+ power_password => "Cisco123!",
+ power_id => "CONTROL-1",
+ add_hosts_entry => true,
+ extra_host_aliases => ["nova","keystone","glance","horizon"],
+ }
+
+cobbler::node { "compute-1":
+ mac => "00:25:b5:00:01:01",
+ profile => "precise-x86_64-auto",
+ ip => "192.168.100.100",
+ domain => "example.lab",
+ preseed => "/etc/cobbler/preseeds/cisco-preseed",
+ power_address => "192.168.100.5:org-EXAMPLE",
+ power_type => "ucs",
+ power_user => "admin",
+ power_password => "Cisco123!",
+ power_id => "COMPUTE-1",
+ }
+# Repeat as necessary.
+}
---
+++ examples/site.pp
@@ -0,0 +1,160 @@
+#
+# base variables and configuration shared by all!
+#
+# Comment out the following for "production", otherwise, you'll see every exec call from every puppet run, which can get a little overwhelming!
+Exec { logoutput => true }
+
+# Need our cobbler definitions
+import "cobbler-node"
+
+# Experimental.  Add a pre-define set of ssh keys to the root account.  This is really only useful for debug purposes, and is _NOT_ the right way to distribute remote access.
+#import "ssh-keys"
+
+#Build Server definition.
+node /build-0/ inherits "cobbler-node" {
+
+#change the servers for your NTP environment
+  class { ntp:
+    servers => [ "ntp.esl.cisco.com", "2.ntp.esl.cisco.com", "3.ntp.esl.cisco.com", ],
+    ensure => running,
+    autoupdate => true,
+  }
+
+# set up a local apt cache.  Eventually this may become a local mirror/repo instead
+  class { apt-cacher-ng:
+    }
+
+# set the right local puppet environment up.  This builds puppetmaster with storedconfigs (and a local mysql instance)
+  class { puppet:
+    run_master => true,
+    puppetmaster_address => $::ipaddress_eth0,
+    mysql_password => 'ubuntu',	
+  }
+}
+
+# base parameters for managed nodes (not necessarily the cobbler/puppetmaster node)
+node base {
+
+  $rabbit_user             = 'rabbit_user'
+  $rabbit_password         = 'rabbit_password'
+  $nova_db_password        = 'nova_db_password'
+  $keystone_db_password    = 'keystone_db_password'
+  $glance_db_password      = 'glance_db_password'
+  $sql_connection          = "mysql://nova:${nova_db_password}@${controller_node_internal}/nova"
+  $admin_password          = 'admin_password'
+  $admin_token             = 'admin_token'
+  $mysql_root_password     = 'sql_pass' 
+
+  $nova_service_password   = 'nova_pass'
+  $glance_service_password = 'glance_pass'
+
+  $admin_email             = 'admin@example.com'
+
+  $public_interface        = 'eth0'
+  $private_interface       = 'eth1'
+ 
+  $fixed_range             = '10.0.0.0/16'
+
+  $verbose                 = true
+
+  class { puppet:
+    run_agent => true,
+    puppetmaster_address => "sdu-os-0.sdu.lab",
+  }
+
+  class { ntp:
+    servers => [ "ntp.esl.cisco.com", "2.ntp.esl.cisco.com", "3.ntp.esl.cisco.com", ],
+    ensure => running,
+    autoupdate => true,
+  }
+
+# This will set up a test nova compute instance script. You need to manually run this if you want to run a quick test on one of the control nodes
+
+  file { '/tmp/test_nova.sh':
+    source => 'puppet:///modules/openstack/nova_test.sh',
+  }
+
+}
+
+# variables related to the flat_DHCP environment
+
+node flat_dhcp inherits base {
+
+  $controller_node_internal = '192.168.100.101'
+  $controller_node_public   = '192.168.100.101' 
+  $sql_connection           = "mysql://nova:${nova_db_password}@${controller_node_internal}/nova"
+
+  class { 'openstack::auth_file': 
+    admin_password       => $admin_password,
+    keystone_admin_token => $admin_token, 
+    controller_node      => $controller_node_internal,
+  }
+
+}
+
+# controller for flat_DHCP
+# NOTE: Change the floating_range
+node /sdu-os-1/ inherits flat_dhcp {
+
+  class { 'nova::volume':
+    enabled => true,
+  }
+
+  class { 'nova::volume::iscsi': }
+
+
+  class { 'openstack::controller':
+    public_address          => $controller_node_public,
+    public_interface        => $public_interface,
+    private_interface       => $private_interface,
+    internal_address        => $controller_node_internal,
+    floating_range          => '192.168.100.64/28',
+    fixed_range             => $fixed_range,
+    multi_host              => false,
+    network_manager         => 'nova.network.manager.FlatDHCPManager',
+    verbose                 => $verbose,
+    mysql_root_password     => $mysql_root_password,
+    admin_email             => $admin_email,
+    admin_password          => $admin_password,
+    keystone_db_password    => $keystone_db_password,
+    keystone_admin_token    => $admin_token,
+    glance_db_password      => $glance_db_password,
+    glance_service_password => $glance_service_password,
+    nova_db_password        => $nova_db_password,
+    nova_service_password   => $nova_service_password,
+    rabbit_password         => $rabbit_password,
+    rabbit_user             => $rabbit_user,
+  }
+
+}
+
+#Build your compute nodes
+node /compute-[1-9]/ inherits flat_dhcp {
+
+#Needed to address a short term failure in nova-volume management - bug has been filed
+  class { 'nova::compute::file_hack': }
+
+  class { 'openstack::compute':
+    private_interface  => $private_interface,
+    internal_address   => $ipaddress_eth0,
+    glance_api_servers => "${controller_node_internal}:9292",
+    rabbit_host        => $controller_node_internal,
+    rabbit_password    => $rabbit_password,
+    rabbit_user        => $rabbit_user,
+    sql_connection     => $sql_connection,
+    vncproxy_host      => $controller_node_internal,
+    verbose            => $verbose,
+ }
+
+}
+
+
+#
+# Default catch-all node definition
+#
+
+node default {
+  notify { 'default_node': }
+}
+
+
