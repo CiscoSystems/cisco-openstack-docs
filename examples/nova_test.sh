44aeb6d74af18338a0c83f6727061f8c25d5a603
Robert Starmer <starmer@cisco.com>
2012-06-03 18:41:11 +0000

    added security group update to nova_test.sh example

--- examples/nova_test.sh
+++ examples/nova_test.sh
@@ -6,7 +6,8 @@ source /root/openrc
 
 # get an image to test with
 #wget http://uec-images.ubuntu.com/releases/11.10/release/ubuntu-11.10-server-cloudimg-amd64-disk1.img
-wget http://192.168.26.163:/precise-server-cloudimg-amd64-disk1.img
+wget http://cloud-images.ubuntu.com/precise/current/precise-server-cloudimg-amd64-disk1.img
+#wget http://128.107.252.163:/precise-server-cloudimg-amd64-disk1.img
 
 # import that image into glance
 glance add name="precise-amd64" is_public=true container_format=ovf disk_format=qcow2 < precise-server-cloudimg-amd64-disk1.img
@@ -23,9 +24,20 @@ IMAGE_ID=`glance index | grep 'precise-amd64' | head -1 |  awk -F' ' '{print $1}
 ssh-keygen -f /tmp/id_rsa -t rsa -N ''
 nova keypair-add --pub_key /tmp/id_rsa.pub key_precise
 
-nova boot --flavor 1 --image ${IMAGE_ID} --key_name key_precise precise_vm
+nova secgroup-create precise_test 'Precise test security group'
+nova secgroup-add-rule precise_test tcp 22 22 0.0.0.0/0
+nova secgroup-add-rule precise_test tcp 80 80 0.0.0.0/0
+nova secgroup-add-rule precise_test icmp -1 -1 0.0.0.0/0
+
+floating_ip=`nova floating-ip-create | grep None | awk '{print $2}'`
+
+nova boot --flavor 1 --security_groups precise_test --image ${IMAGE_ID} --key_name key_precise precise_vm
 
 nova show precise_vm
 
-# create ec2 credentials
-keystone ec2-credentials-create
+nova add-floating-ip precise_vm $floating_ip
+# wait for the server to boot
+
+sleep 60
+ssh ubuntu@$floating_ip -i /tmp/id_rsa
+
