3a4b2c1f946b60f13571f2fec113fb03c31d9290
Robert Starmer <starmer@cisco.com>
2012-06-18 18:25:36 +0000

    udated sample site_manifests/sdu-dmz

--- site_manifests/sdu-dmz/cobbler-node.pp
+++ site_manifests/sdu-dmz/cobbler-node.pp
@@ -40,25 +40,28 @@ node /cobbler-node/ {
  }
 
  cobbler::ubuntu::preseed { "cisco-preseed":
-  packages => "openssh-server lvm2 libvirt-bin ntp puppet kvm",
+  packages => "openssh-server vlan lvm2 ntp puppet",
+  early_command => 'dd if=/dev/zero of=/dev/sda count=1000 bs=1M',
   late_command => '
 sed -e "/logdir/ a pluginsync=true" -i /target/etc/puppet/puppet.conf ; \
 sed -e "s/START=no/START=yes/" -i /target/etc/default/puppet ; \
+echo "8021q" >> /target/etc/modules ; \
 echo "server ${http_server} iburst" > /target/etc/ntp.conf ; \
-echo "auto eth1" >> /target/etc/network/interfaces ; \
-echo "iface eth1 inet loopback" >> /target/etc/network/interfaces
+echo "auto eth0.400" >> /target/etc/network/interfaces ; \
+echo "iface eth0.400 inet loopback" >> /target/etc/network/interfaces ; \
+echo "   vlan-raw-device eth0" >> /target/etc/network/interfaces
 ',
   proxy => 'http://192.168.100.254:3142/',
   password_crypted => '$6$5NP1.NbW$WOXi0W1eXf9GOc0uThT5pBNZHqDH9JNczVjt9nzFsH7IkJdkUpLeuvBU.Zs9x3P6LBGKQh6b0zuR8XSlmcuGn.',
   expert_disk => true,
-  diskpart => ['/dev/sdc'],
-  boot_disk => '/dev/sdc',
+  diskpart => ['/dev/sda'],
+  boot_disk => '/dev/sda',
  }
 
 
 # cobbler node definitions
 cobbler::node { "sdu-os-1":
- mac => "00:25:b5:00:00:08",
+ mac => "00:25:B5:0A:00:5F",
  profile => "precise-x86_64-auto",
  ip => "192.168.100.101",
  domain => "sdu.lab",
@@ -73,7 +76,7 @@ cobbler::node { "sdu-os-1":
  }
 
 cobbler::node { "sdu-os-2":
- mac => "00:25:b5:00:00:16",
+ mac => "00:25:B5:0A:00:3F",
  profile => "precise-x86_64-auto",
  ip => "192.168.100.102",
  domain => "sdu.lab",
@@ -86,7 +89,7 @@ cobbler::node { "sdu-os-2":
  }
 
 cobbler::node { "sdu-os-3":
- mac => "00:25:b5:00:00:05",
+ mac => "00:25:B5:0A:00:1F",
  profile => "precise-x86_64-auto",
  ip => "192.168.100.103",
  domain => "sdu.lab",
@@ -99,7 +102,7 @@ cobbler::node { "sdu-os-3":
  }
 
 cobbler::node { "sdu-os-4":
- mac => "00:25:b5:00:00:04",
+ mac => "00:25:B5:0A:00:7E",
  profile => "precise-x86_64-auto",
  ip => "192.168.100.104",
  domain => "sdu.lab",
@@ -112,7 +115,7 @@ cobbler::node { "sdu-os-4":
  }
 
 cobbler::node { "sdu-os-5":
- mac => "00:25:b5:00:00:03",
+ mac => "00:25:B5:0A:00:5E",
  profile => "precise-x86_64-auto",
  ip => "192.168.100.105",
  domain => "sdu.lab",
@@ -124,29 +127,16 @@ cobbler::node { "sdu-os-5":
  power_id => "SDU-OS-5",
  }
 
-#cobbler::node { "sdu-os-6":
-# mac => "00:25:b5:00:00:02",
-# profile => "precise-x86_64-auto",
-# ip => "192.168.100.106",
-# domain => "sdu.lab",
-# preseed => "/etc/cobbler/preseeds/cisco-preseed",
-# power_address => "192.168.26.15:org-SDU",
-# power_type => "ucs",
-# power_user => "admin",
-# power_password => "Sdu!12345",
-# power_id => "SDU-OS-6",
-# }
-
-cobbler::node { "sdu-os-7":
- mac => "00:25:b5:00:00:01",
+cobbler::node { "sdu-os-6":
+ mac => "00:25:B5:0A:00:3E",
  profile => "precise-x86_64-auto",
- ip => "192.168.100.107",
+ ip => "192.168.100.106",
  domain => "sdu.lab",
  preseed => "/etc/cobbler/preseeds/cisco-preseed",
  power_address => "192.168.26.15:org-SDU",
  power_type => "ucs",
  power_user => "admin",
  power_password => "Sdu!12345",
- power_id => "SDU-OS-7",
+ power_id => "SDU-OS-6",
  }
 }
---
+++ site_manifests/sdu-dmz/no-swift-site.pp
@@ -0,0 +1,193 @@
+#
+# base variables and configuration shared by all!
+#
+Exec { logoutput => true }
+
+import "cobbler-node"
+import "ssh-keys"
+
+#include "lvm"
+
+node /sdu-os-0/ inherits "cobbler-node" {
+
+  class { ntp:
+    servers => [ "ntp.esl.cisco.com", "2.ntp.esl.cisco.com", "3.ntp.esl.cisco.com", ],
+    ensure => running,
+    autoupdate => true,
+  }
+
+  class { apt-cacher-ng:
+    }
+
+  class { puppet:
+    run_master => true,
+    puppetmaster_address => $::ipaddress_eth0,
+    mysql_password => 'ubuntu',	
+  }
+}
+
+node base {
+
+#  include nagios::target
+
+  $rabbit_user             = 'rabbit_user'
+  $rabbit_password         = 'rabbit_password'
+  $nova_db_password        = 'nova_db_password'
+  $keystone_db_password    = 'keystone_db_password'
+  $glance_db_password      = 'glance_db_password'
+  $sql_connection          = "mysql://nova:${nova_db_password}@${controller_node_internal}/nova"
+  $admin_password          = 'admin_password'
+  $admin_token             = 'admin_token'
+  $mysql_root_password     = 'sql_pass' 
+
+  $nova_service_password   = 'nova_pass'
+  $glance_service_password = 'glance_pass'
+
+  $admin_email             = 'admin@sdu.lab'
+
+  $public_interface        = 'eth0'
+  $private_interface       = 'eth0.400'
+  
+  $fixed_range             = '10.0.0.0/16'
+
+  $multi_host		   = false
+  $verbose                 = true
+
+  # this is a dmz specific hack
+  #include cisco_dmz
+  #include ssh
+
+  #exec { "networking-refresh":
+  #  command     => "/sbin/ifup eth1",
+  #  #refreshonly => "true",
+  #}
+
+  # this is required to make concat work with PE
+  #file { '/var/lib/puppet':
+  #  ensure => directory,
+  #}
+
+  class { puppet:
+    run_agent => true,
+    puppetmaster_address => "sdu-os-0.sdu.lab",
+  }
+
+  class { ntp:
+    servers => [ "ntp.esl.cisco.com", "2.ntp.esl.cisco.com", "3.ntp.esl.cisco.com", ],
+    ensure => running,
+    autoupdate => true,
+  }
+
+  file { '/tmp/test_nova.sh':
+    source => 'puppet:///modules/openstack/nova_test.sh',
+  }
+
+}
+
+# variables related to the multi_host vlan environemnt
+
+node flat_dhcp inherits base {
+
+  $controller_node_internal = '192.168.100.101'
+  $controller_node_public   = '192.168.100.101' 
+  $sql_connection           = "mysql://nova:${nova_db_password}@${controller_node_internal}/nova"
+
+  class { 'openstack::auth_file': 
+    admin_password       => $admin_password,
+    keystone_admin_token => $admin_token, 
+    controller_node      => $controller_node_internal,
+  }
+
+}
+
+# controller for multi-host with DHCP
+node /sdu-os-1/ inherits flat_dhcp {
+
+  class { 'nova::volume':
+    enabled => true,
+  }
+
+  class { 'nova::volume::iscsi': }
+
+
+  class { 'openstack::controller':
+    public_address          => $controller_node_public,
+    public_interface        => $public_interface,
+    private_interface       => $private_interface,
+    internal_address        => $controller_node_internal,
+    floating_range          => '192.168.100.64/28',
+    fixed_range             => $fixed_range,
+    multi_host              => $multi_host,
+    network_manager         => 'nova.network.manager.FlatDHCPManager',
+    verbose                 => $verbose,
+    mysql_root_password     => $mysql_root_password,
+    admin_email             => $admin_email,
+    admin_password          => $admin_password,
+    keystone_db_password    => $keystone_db_password,
+    keystone_admin_token    => $admin_token,
+    glance_db_password      => $glance_db_password,
+    glance_service_password => $glance_service_password,
+    nova_db_password        => $nova_db_password,
+    nova_service_password   => $nova_service_password,
+    rabbit_password         => $rabbit_password,
+    rabbit_user             => $rabbit_user,
+    export_resources        => false,
+  }
+
+#  class { 'role_swift_ringbuilder': }
+
+#  class { 'role_swift_proxy':
+#    require => Class['role_swift_ringbuilder'],
+#  }
+
+}
+
+
+node /sdu-os-2/ inherits flat_dhcp {
+
+  class { 'nova::compute::file_hack': }
+
+  class { 'openstack::compute':
+    private_interface  => $private_interface,
+    internal_address   => $ipaddress_eth0,
+    glance_api_servers => "${controller_node_internal}:9292",
+    rabbit_host        => $controller_node_internal,
+    rabbit_password    => $rabbit_password,
+    rabbit_user        => $rabbit_user,
+    sql_connection     => $sql_connection,
+    vncproxy_host      => $controller_node_internal,
+    verbose            => $verbose,
+    manage_volumes     => true,
+ }
+
+}
+
+
+node /sdu-os-[3-4]/ inherits flat_dhcp {
+
+  class { 'nova::compute::file_hack': }
+
+  class { 'openstack::compute':
+    private_interface  => $private_interface,
+    internal_address   => $ipaddress_eth0,
+    glance_api_servers => "${controller_node_internal}:9292",
+    rabbit_host        => $controller_node_internal,
+    rabbit_password    => $rabbit_password,
+    rabbit_user        => $rabbit_user,
+    sql_connection     => $sql_connection,
+    vncproxy_host      => $controller_node_internal,
+    verbose            => $verbose,
+    manage_volumes     => false,
+ }
+
+}
+
+#
+# all in one examples
+#
+
+node default {
+  notify { 'default_node': }
+}
+
+
