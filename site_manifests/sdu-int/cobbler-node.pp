a9c11ebcd1375a9c21c27a2db23f68d7e4d6ac8c
Robert Starmer <starmer@cisco.com>
2012-06-22 12:14:14 -0700

    changed internal site to include promisc model

--- site_manifests/sdu-int/cobbler-node.pp
+++ site_manifests/sdu-int/cobbler-node.pp
@@ -1,5 +1,6 @@
 node /cobbler-node/ {
 
+
 # set up the cobbler instance on the "cobbler" node
  class { cobbler:
   node_subnet => "10.16.205.0",	# this is the IP domain
@@ -20,24 +21,25 @@ node /cobbler-node/ {
  }
 
  cobbler::ubuntu::preseed { "cisco-preseed":
-  packages => "openssh-server libvirt-bin ntp puppet kvm",
+  packages => "openssh-server vlan lvm2 ntp puppet",
   late_command => '
 sed -e "/logdir/ a pluginsync=true" -i /target/etc/puppet/puppet.conf ; \
 sed -e "s/START=no/START=yes/" -i /target/etc/default/puppet ; \
-echo "server ${http_server} iburst" > /target/etc/ntp.conf i ; \
-echo "auto eth1" >> /target/etc/network/interfaces ; \
-echo "iface eth1 inet loopback" >> /target/etc/network/interfaces ; \
+echo "8021q" >> /target/etc/modules ; \
+echo "server ${http_server} iburst" > /target/etc/ntp.conf ; \
+echo -e "auto eth0.400\niface eth0.400 inet loopback\n\tvlan-raw-device eth0\n\tup ip link set eth0.400 promisc on" >> /target/etc/network/interfaces ; \
+true
 ',
-  proxy => 'http://128.107.252.163:3142/',
+  proxy => 'http://10.16.205.60:3142/',
   password_crypted => '$6$5NP1.NbW$WOXi0W1eXf9GOc0uThT5pBNZHqDH9JNczVjt9nzFsH7IkJdkUpLeuvBU.Zs9x3P6LBGKQh6b0zuR8XSlmcuGn.',
   expert_disk => true,
-  diskpart => ['/dev/sda','/dev/sdb','/dev/sdc','/dev/sdd'],
-  boot_disk => '/dev/sdc',
+  diskpart => ['/dev/sda'],
+  boot_disk => '/dev/sda',
  }
 
 # cobbler node definitions
-  cobbler::node { "sdu-os-1":
-    mac => "00:25:B5:00:05:EF",
+  cobbler::node { "control-1":
+    mac => "00:25:B5:00:05:BF",
     profile => "precise-x86_64-auto",
     ip => "10.16.205.61",
     domain => "sdu.lab",
@@ -46,13 +48,11 @@ echo "iface eth1 inet loopback" >> /target/etc/network/interfaces ; \
     power_type => "ucs",
     power_user => "admin",
     power_password => "Ciscoese123",
-    power_id => "SDU-OS-1",
-    boot_disk => "/dev/sdc",
-    add_hosts_entry => true,
-    extra_host_aliases => ["nova","keystone","glance","horizon"],
+    power_id => "SDU-OS-2",
+    boot_disk => "/dev/sda",
   }
-  cobbler::node { "sdu-os-2":
-    mac => "00:25:B5:00:05:BF",
+  cobbler::node { "compute-1":
+    mac => "00:25:B5:00:05:AF",
     profile => "precise-x86_64-auto",
     ip => "10.16.205.62",
     domain => "sdu.lab",
@@ -61,11 +61,11 @@ echo "iface eth1 inet loopback" >> /target/etc/network/interfaces ; \
     power_type => "ucs",
     power_user => "admin",
     power_password => "Ciscoese123",
-    power_id => "SDU-OS-2",
-    boot_disk => "/dev/sdc",
+    power_id => "SDU-OS-3",
+    boot_disk => "/dev/sda",
   }
-  cobbler::node { "sdu-os-3":
-    mac => "00:25:B5:00:05:AF",
+  cobbler::node { "compute-2":
+    mac => "00:25:B5:00:05:9F",
     profile => "precise-x86_64-auto",
     ip => "10.16.205.63",
     domain => "sdu.lab",
@@ -74,47 +74,8 @@ echo "iface eth1 inet loopback" >> /target/etc/network/interfaces ; \
     power_type => "ucs",
     power_user => "admin",
     power_password => "Ciscoese123",
-    power_id => "SDU-OS-3",
-    boot_disk => "/dev/sdc",
-  }
-  cobbler::node { "sdu-os-4":
-    mac => "00:25:B5:00:05:9F",
-    profile => "precise-x86_64-auto",
-    ip => "10.16.205.64",
-    domain => "sdu.lab",
-    preseed => "/etc/cobbler/preseeds/cisco-preseed",
-    power_address => "10.16.205.20",
-    power_type => "ucs",
-    power_user => "admin",
-    power_password => "Ciscoese123",
     power_id => "SDU-OS-4",
-    boot_disk => "/dev/sdc",
-  }
-  cobbler::node { "sdu-os-5":
-    mac => "00:25:B5:00:05:7F",
-    profile => "precise-x86_64-auto",
-    ip => "10.16.205.65",
-    domain => "sdu.lab",
-    preseed => "/etc/cobbler/preseeds/cisco-preseed",
-    power_address => "10.16.205.20",
-    power_type => "ucs",
-    power_user => "admin",
-    power_password => "Ciscoese123",
-    power_id => "SDU-OS-5",
-    boot_disk => "/dev/sdc",
-  }
-  cobbler::node { "sdu-os-6":
-    mac => "00:25:B5:00:05:5F",
-    profile => "precise-x86_64-auto",
-    ip => "10.16.205.66",
-    domain => "sdu.lab",
-    preseed => "/etc/cobbler/preseeds/cisco-preseed",
-    power_address => "10.16.205.20",
-    power_type => "ucs",
-    power_user => "admin",
-    power_password => "Ciscoese123",
-    power_id => "SDU-OS-6",
-    boot_disk => "/dev/sdc",
+    boot_disk => "/dev/sda",
   }
 
 }
