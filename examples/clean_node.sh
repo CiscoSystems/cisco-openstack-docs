a1742bc484a46182dc59b5ebf55a9e5b70815688
Daneyon Hansen <danehans@cisco.com>
2012-08-13 12:45:17 -0600

    changed cert clean to node clean
    node clean removes host db entry for puppet storedconfigs.

--- examples/clean_node.sh
+++ examples/clean_node.sh
@@ -9,6 +9,8 @@ sudo cobbler system edit --name=$1 --netboot-enable=Y
 sudo cobbler system poweroff --name=$1
 sleep 5
 sudo cobbler system poweron --name=$1
-sudo puppet cert clean $1.$domain
+#Changed cert clean to clean node so node is removed from stored configs db
+#sudo puppet cert clean $1.$domain
+sudo puppet node clean $1.$domain
 sudo ssh-keygen -R $1
 sudo ssh-keygen -R `host $1 | awk '{print \$4}'`
